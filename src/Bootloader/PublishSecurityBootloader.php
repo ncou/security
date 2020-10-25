5e3c000250d7f92f67b8bbc5e0c4aee3be52f6a8
ncou <nicolas_courade@yahoo.fr>
2020-10-25 16:27:01 +0100

    update

---
+++ src/Bootloader/PublishSecurityBootloader.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Chiron\Security\Bootloader;
+
+use Chiron\Core\Directories;
+use Chiron\Bootload\AbstractBootloader;
+use Chiron\PublishableCollection;
+
+final class PublishSecurityBootloader extends AbstractBootloader
+{
+    public function boot(PublishableCollection $publishable, Directories $directories): void
+    {
+        // copy the configuration file template from the package "config" folder to the user "config" folder.
+        $publishable->add(__DIR__ . '/../../config/security.php.dist', $directories->get('@config/security.php'));
+    }
+}
